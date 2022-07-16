a533bca53a2eafa49b2f8a4f4e4b83ca7ecf26ae
its2akshay <105869651+its2akshay@users.noreply.github.com>
2022-07-16 21:57:00 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be
+integer or string and expressions will be mathematical operators.
+*
+'hello'
+-87.8
+-
+/
++
+6Values: 'hello', -87.8, 6
+Expressions: *, -, /, +2. What is the difference between string and variable?String is a type of data and variable is a storage location3. Describe three different data types.Integers: They are whole numbers 
+Lists: They are collection of data inside square breackets and are mutable (e.g., [23, 'str', 4234, 78.90]
+Boolean: They return True or false 4. What is an expression made up of? What do all expressions do?Expression is made up of values, operators and variables. It produces a result5. This assignment statements, like spam = 10. What is the difference between an
+expression and a statement?Expression is made up of values, operators and variables that produces a result.
+A statement is a line of instruction that the python executes (e.g., assignment statement)6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1Thus bacon = 237. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3Both the terms returns the same value 'spamspamspam'8. Why is eggs a valid variable name while 100 is invalid?Python does not allow variable names to start with numbers 9. What three functions can be used to get the integer, floating-point number, or string
+version of a value?Type casting functions such as int(), float() and str() can be used respectively 10. Why does this expression cause an error? How can you fix it?
+'I have eaten'+ 99 + 'burritos'Because a string type element cannot be concatenated with a integer type element 
+we can fix the issue by type casting the int to str and then concatenate the elements
